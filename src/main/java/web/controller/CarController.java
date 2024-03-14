72719a015cdbe8bf806933c62ba352f6a1c6ec81
Степанова_Елизавета <stepanowa.liza2001@gmail.com>
2024-03-14 17:21:05 +0300

    Task 2.2.2 update

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,18 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceIml;
+import web.service.CarService;
 
 
 @Controller
 public class CarController {
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
 
     @GetMapping(value = "/cars")
     public String getString(@RequestParam(value = "count", defaultValue = "5") int count,
                             Model model) {
-        CarServiceIml cars = new CarServiceIml();
-        var carsData = cars.getCar(count);
-        model.addAttribute("cars", carsData);
+        model.addAttribute("cars", carService.getCar(count));
         return "cars";
     }
 }
--- src/main/java/web/service/CarServiceIml.java
+++ src/main/java/web/service/CarServiceIml.java
@@ -1,14 +1,26 @@
 package web.service;
 
-import web.dao.CarDaoImp;
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class CarServiceIml implements CarService{
+@Component
+public class CarServiceIml implements CarService {
+    private final List<Car> cars;
+
+    public CarServiceIml() {
+        List<Car> cars = new ArrayList<>();
+        cars.add( new Car("Lada", "Y489EB136", "green"));
+        cars.add( new Car("Kia", "P346JF013", "white"));
+        cars.add( new Car("Opel", "H578MD103", "black"));
+        cars.add( new Car("Haval", "J085UF035", "green"));
+        cars.add( new Car("Mercedes", "A555AA101", "red"));
+        this.cars = cars;
+    }
     @Override
     public List<Car> getCar(int count) {
-        CarDaoImp carDaoImp = new CarDaoImp();
-        return carDaoImp.getCountCar(count);
+        return cars.stream().limit(count).toList();
     }
 }
